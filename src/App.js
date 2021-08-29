2a6372cc2dcda9326567d5254af4efd5fd46ea40
Esteban Damazio <edamazzio@gmail.com>
2021-08-28 19:50:16 -0600

    Add sign in and form input components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/homepage/homepage'
 import {Route, Switch} from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={Homepage}/>
                 <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInSignUp}/>
             </Switch>
         </div>
     );
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './menu-item.styles.scss'
+import './menu-item.scss'
 import {withRouter} from "react-router-dom";
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
